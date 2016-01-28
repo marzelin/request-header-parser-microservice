84baeebb0c40c24d0d5c1014b9018dd7febee022
marzelin <marzelin@gamil.com>
2016-01-28 17:52:31 +0100

    add app services to return json with user ip, lang and software

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+app.get('/', function getCb(req, res) {
+  var ip6and4 = req.ip;
+  var ip4regExp = /(?:\d{1,3}\.){3}\d{1,3}/;
+  var ip = ip6and4.match(ip4regExp)[0];
+
+  var headers = req.headers;
+
+  var acceptLanguageField = headers['accept-language'];
+  var firstValueRegExp = /[^,]*/
+  var language = acceptLanguageField.match(firstValueRegExp)[0];
+
+  var userAgent = headers['user-agent'];
+  var valueInParenthesesRegExp = /(\()([^)]*)(\))/;
+  var software = userAgent.match(valueInParenthesesRegExp)[2];
+
+  var retObj = {
+    ipaddress: ip,
+    language: language,
+    software: software
+  }
+
+  res.json(retObj);
+});
+
+app.listen(port);
